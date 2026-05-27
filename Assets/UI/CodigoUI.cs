2bed7de9498b1197d683557d6eeee02678a447ac
Eric Díaz Bueno <ericdiaz@ceroca.cat>
2026-05-27 11:19:36 +0200

    Extra 1- posar tutorial al menu d'inici

--- Assets/UI/CodigoUI.cs
+++ Assets/UI/CodigoUI.cs
@@ -9,9 +9,34 @@ public class CodigoInici : MonoBehaviour
         var DocumentUI = GetComponent<UIDocument>();
         VisualElement root = DocumentUI.rootVisualElement;
         Button buttonNewGame = root.Q<Button>("NewGame");
+        Button buttonContinue = root.Q<Button>("Continue");
+        Button buttonExit = root.Q<Button>("Exit");
+
+        VisualElement tutorialPanel = root.Q<VisualElement>("TutorialPanel");
+        Button buttonCloseTutorial = root.Q<Button>("CloseTutorial");
+
+        // Nou Joc
         buttonNewGame.clicked += () =>
         {
             SceneManager.LoadScene("CasaEldrin");
+        };
+
+        // Com Jugar? (Obrir tutorial)
+        buttonContinue.clicked += () =>
+        {
+            tutorialPanel.style.display = DisplayStyle.Flex;
+        };
+
+        // Tancar Tutorial
+        buttonCloseTutorial.clicked += () =>
+        {
+            tutorialPanel.style.display = DisplayStyle.None;
+        };
+
+        // Sortir
+        buttonExit.clicked += () =>
+        {
+            Application.Quit();
         };
     }
 }
